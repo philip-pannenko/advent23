715ee35b9e6e2687e3e4dc67b360d49e1d9e1206
philip-pannenko <philip.pannenko@gmail.com>
2023-12-02 21:55:50 -0500

    day2b

--- day2/day2.py
+++ day2/day2.py
@@ -1,17 +1,25 @@
 import re
+from functools import reduce
 
-max_cubes = {
+MAX_CUBES = {
     'red': 12,
     'green': 13,
     'blue': 14
-
 }
+
+CUBE_INDEX = {
+    'red': 0,
+    'green': 1,
+    'blue': 2
+}
+
+IS_PART1 = False
 
 
 def is_game_possible(cubes: list[str]):
     for cube in cubes:
         [quantity, color] = cube.strip().split(' ')
-        if int(quantity) > int(max_cubes[color]):
+        if int(quantity) > int(MAX_CUBES[color]):
             return False
     return True
 
@@ -25,16 +33,26 @@ def func():
             is_possible = True
             [game_id, all_games] = line.split(': ')
             games = all_games.split(';')
+            minimum = [0, 0, 0]
 
             for game in games:
                 cubes = game.split(',')
 
-                if not is_game_possible(cubes):
-                    is_possible = False
-                    break
+                if IS_PART1:
+                    if not is_game_possible(cubes):
+                        is_possible = False
+                        break
+                else:
+                    for cube in cubes:
+                        [quantity, color] = cube.strip().split(' ')
+                        if minimum[CUBE_INDEX[color]] < int(quantity):
+                            minimum[CUBE_INDEX[color]] = int(quantity)
 
-            if is_possible:
-                total += int(re.findall('(?:[\d]+)', game_id)[0])
+            if IS_PART1:
+                if is_possible:
+                    total += int(re.findall('(?:[\d]+)', game_id)[0])
+            else:
+                total += reduce(lambda x, y: x*y, minimum)
 
         print(total)
 
